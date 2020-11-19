85f06745b942a7045e0f4360e368d21e554e23c0
akansha-n888 <74415318+akansha-n888@users.noreply.github.com>
2020-11-19 09:51:59 -0500

    Update Project830_task1.ino

--- Project830_task1.ino
+++ Project830_task1.ino
@@ -1,42 +1,73 @@
 
-/* Robot contains 3 caster wheels, 1 fixed wheel, 1 DC motor and 1 servo.
+/* Robot contains 3 caster wheels, 2 fixed wheels, 1 DC motor and 1 stepper.
 The following program is the attributed tot Task 1 (manual control)*/
 
-//servo parameters
-#include <Servo.h>
-Servo myservo;
+#include "IRremote.h"
+#include <Stepper.h>
+
+const int stepsPerRevolution = 2048;
+Stepper stepper = Stepper(stepsPerRevolution, 11, 9, 10, 8);                  //might have to change depending on your set up (i.e. 8,9,10,11)
 
 //DC motor paramters
 #define ENABLE 5
 #define DIRA 3
 #define DIRB 4
 
+int receiver = 12;
+IRrecv irrecv(receiver);    // create instance of 'irrecv'
+decode_results results;  
+
 void setup() {
-   myservo.attach(7);
-   myservo.write(90);
 
   pinMode(ENABLE,OUTPUT);
   pinMode(DIRA,OUTPUT);
   pinMode(DIRB,OUTPUT);
+
+  stepper.step(0);                  
+  stepper.setSpeed(10);   
+
+  irrecv.enableIRIn();
+  
   Serial.begin(9600);
 
 }
 
-void loop() {
-  //needs to include remote commands to set conditions for the servo and DC motor
+void loop() 
 
-  // move servo 90 deg cw for right turn
-  myservo.write(0);
-  //move servo 90 deg ccw for left turn
-  myservo.write(180);
+{
+if (irrecv.decode(&results))                             // have we received an IR signal?
 
-  digitalWrite(ENABLE,HIGH); // enable on
-  digitalWrite(DIRA,HIGH); //one way
-  digitalWrite(DIRB,LOW);
-  delay(500);
-  
-  digitalWrite(DIRA,LOW);  //reverse
-  digitalWrite(DIRB,HIGH);
-  delay(500);
+  {
+    switch(results.value)
 
+    {
+
+      case 0xFFA857:                                     // VOL+ button pressed
+        digitalWrite(ENABLE,HIGH);                       // enable on
+        digitalWrite(DIRA,HIGH);                         //one way
+        digitalWrite(DIRB,LOW);
+        delay(500); 
+                      break;
+
+      case 0xFF629D:                                     // VOL- button pressed
+        digitalWrite(DIRA,LOW);                          //reverse
+        digitalWrite(DIRB,HIGH);
+        delay(500);
+                      break;
+
+      case 0xFFC23D:                                     // Forward Skip button pressed
+          stepper.step(1000);                            //cw
+          delay(50);     
+
+      case 0xFF22DD:                                     // Backward Skip button pressed          
+          stepper.step(-1000);                           //ccw
+          delay(500);
+    }
+    
+      irrecv.resume(); // receive the next value
+                 digitalWrite(8, LOW);
+                 digitalWrite(9, LOW);
+                 digitalWrite(10, LOW);
+                 digitalWrite(11, LOW);       
+  }  
 }
