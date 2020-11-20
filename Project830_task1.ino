cfc8404082e4c2d49bea5c2c02f8cf06d621bc75
akansha-n888 <74415318+akansha-n888@users.noreply.github.com>
2020-11-20 00:14:45 -0500

    Update Project830_task1.ino

--- Project830_task1.ino
+++ Project830_task1.ino
@@ -1,27 +1,19 @@
-
-/* Robot contains 3 caster wheels, 2 fixed wheels, 1 DC motor and 1 stepper.
-The following program is the attributed tot Task 1 (manual control)*/
-
 #include "IRremote.h"
 #include <Stepper.h>
+#include <Servo.h>
+
+Servo myservo;
 
 const int stepsPerRevolution = 2048;
 Stepper stepper = Stepper(stepsPerRevolution, 8, 9, 10, 11);                 
 
-//DC motor paramters
-#define ENABLE 5
-#define DIRA 3
-#define DIRB 4
 
 int receiver = 12;
 IRrecv irrecv(receiver);    // create instance of 'irrecv'
 decode_results results;  
 
 void setup() {
-
-  pinMode(ENABLE,OUTPUT);
-  pinMode(DIRA,OUTPUT);
-  pinMode(DIRB,OUTPUT);
+  myservo.attach(7);
 
   stepper.step(0);                  
   stepper.setSpeed(10);   
@@ -42,25 +34,22 @@ if (irrecv.decode(&results))                             // have we received an 
 
     {
 
-      case 0xFFA857:                                     // VOL+ button pressed
-        digitalWrite(ENABLE,HIGH);                       // enable on
-        digitalWrite(DIRA,HIGH);                         //one way
-        digitalWrite(DIRB,LOW);
-        delay(500); 
+      case 0xFFA857:                                     // VOL+ button pressed - move system forward with stepper
+      stepper.step(2048);
+      delay(10);
                       break;
 
-      case 0xFF629D:                                     // VOL- button pressed
-        digitalWrite(DIRA,LOW);                          //reverse
-        digitalWrite(DIRB,HIGH);
-        delay(500);
+      case 0xFF629D:                                     // VOL- button pressed - move system backward with stepper
+      stepper.step(-2048);
+      delay(10);
                       break;
 
       case 0xFFC23D:                                     // Forward Skip button pressed
-          stepper.step(1000);                            //cw
+          myservo.write(180);                            //cw
           delay(50);     
 
       case 0xFF22DD:                                     // Backward Skip button pressed          
-          stepper.step(-1000);                           //ccw
+          myservo.write(0);                                  //ccw
           delay(500);
     }
     
