aef6d695e246a022a8eed192be58b64f2b2f066d
aporojan <45049351+aporojan@users.noreply.github.com>
2020-11-23 22:55:16 -0500

    Added IR codes for each button on the remote

--- RemoteCodes.ino
+++ RemoteCodes.ino
@@ -1,4 +1,16 @@
-/* sample IR remote code*/
+/*
+
+IR codes (same layout as the remote):
+
+  FFA25D - Power Button     FF629D - VOL+           FFE21D - FUNC/STOP
+  FF22DD - Backward         FF02FD - Play/Pause     FFC23D - Forward
+  FFE01F - Down Arrow       FFA857 - VOL-           FF906F - Up Arrow
+  FF6897 - 0                FF9867 - EQ             FFB04F - ST/REPT
+  FF30CF - 1                FF18E7 - 2              FF7A85 - 3
+  FF10EF - 4                FF38C7 - 5              FF5AA5 - 6
+  FF42BD - 7                FF4AB5 - 8              FF52AD - 9
+
+*/
 
 #include <IRremote.h>
 
